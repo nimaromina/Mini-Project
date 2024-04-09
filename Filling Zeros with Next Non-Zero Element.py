aa3c7e78fab9ebb8ac1fff220dd3413aace115fa
nima romina <165555241+nimaromina@users.noreply.github.com>
2024-04-09 22:30:56 +0430

    Update Filling Zeros with Next Non-Zero Element.py

--- Filling Zeros with Next Non-Zero Element.py
+++ Filling Zeros with Next Non-Zero Element.py
@@ -1,8 +1,18 @@
 lst = [0,1,0,0,2,3,6,0,0,7,6,4,0,6]
-last = lst[-1]
-for i in range(len(lst)-1,-1,-1):
-    if lst[i] ==0:
-        lst[i] =last
-        continue
-    last = lst[i]
+def nim(lst):
+    if lst[i] == 0:
+        lst[i] = lst[i+1]
+        if lst[i+1] == 0:
+            nim(lst)
+    return lst
+
+
+
+for i in range(len(lst)):
+    if lst[i] == 0:
+        nim(lst)
+        
 print(lst)
+
+    
+   
